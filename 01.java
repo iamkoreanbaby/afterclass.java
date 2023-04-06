044223b9bbe61f3e467944e6f66db7a3bcca9138
Sejun <117452677+iamkoreanbaby@users.noreply.github.com>
2023-04-06 18:20:19 +0900

    Create 01.java

---
+++ 01.java
@@ -0,0 +1,13 @@
+package ksj0406;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(3 + 5);
+		System.out.println("3 + 5");
+		System.out.println("3 + 5 = " + (3+5));	
+	}			
+																									
+}
+	
